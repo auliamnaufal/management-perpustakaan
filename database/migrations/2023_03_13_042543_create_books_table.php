5395e9100c5964b47a321ac7273bf3783248a368
auliamnaufal <auliamnaufal@gmail.com>
2023-03-13 14:37:05 +0700

    Feature: create books, shelves, and categories migration

---
+++ database/migrations/2023_03_13_042543_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn', 13)->unique();
+            $table->string('title');
+            $table->string('cover')->nullable();
+            $table->string('author');
+            $table->foreignId('shelf_id')->nullable()->constrained();
+            $table->foreignId('category_id')->nullable()->constrained();
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
