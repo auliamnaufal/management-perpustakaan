08e5f5893210957fac8b04774adc3a83392bdb6f
auliamnaufal <auliamnaufal@gmail.com>
2023-03-13 14:43:05 +0700

    Feature: run factory for each model instance

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Shelf;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,11 @@ class DatabaseSeeder extends Seeder
          ]);
 
          \App\Models\User::factory(10)->create();
+
+         Category::factory(10)->create();
+
+         Shelf::factory(10)->create();
+
+         Book::factory(50)->create();
     }
 }
