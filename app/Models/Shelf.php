10f684e6e62c45703a54cc8305459f2337b1610f
auliamnaufal <auliamnaufal@gmail.com>
2023-03-14 13:31:34 +0700

    Feature: specify fillable and books relation at shelf
    set name field to fillable property and initialize books relationship from shelf model

--- app/Models/Shelf.php
+++ app/Models/Shelf.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Shelf extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function books() {
+        return $this->hasMany(Book::class);
+    }
 }
