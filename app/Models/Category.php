a0ed01828104d711bf3b540f7b2a3fe5015c2778
auliamnaufal <auliamnaufal@gmail.com>
2023-03-14 11:40:31 +0700

    Refactor: specify fillable field for category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,10 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name'
+    ];
+
     public function books() {
         return $this->hasMany(Book::class);
     }
