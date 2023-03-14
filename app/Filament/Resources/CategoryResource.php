14ab5db8eea3ed245fb5d85e266dce97717652c5
auliamnaufal <auliamnaufal@gmail.com>
2023-03-14 13:19:29 +0700

    Feature: change category resource menu icon to beaker

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -17,7 +17,7 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-beaker';
 
     public static function form(Form $form): Form
     {
