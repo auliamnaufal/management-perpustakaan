150b0d3144f8e07fc1b1fd747412a884cc7844b6
auliamnaufal <auliamnaufal@gmail.com>
2023-03-14 13:31:52 +0700

    Feature: create shelf page

---
+++ app/Filament/Resources/ShelfResource.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ShelfResource\Pages;
+use App\Filament\Resources\ShelfResource\RelationManagers;
+use App\Models\Shelf;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ShelfResource extends Resource
+{
+    protected static ?string $model = Shelf::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->unique()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('books_count')->counts('books')
+                    ->url(fn(Shelf $record) => BookResource::getUrl('index', ['tableFilters[shelf_id][value]' => $record->id]))
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageShelves::route('/'),
+        ];
+    }
+}
