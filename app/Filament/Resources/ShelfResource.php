f7bd63b0b6f5c56b415154f26110c240d3d96472
auliamnaufal <auliamnaufal@gmail.com>
2023-03-15 08:32:17 +0700

    Feature: change shelf menu icon to table

--- app/Filament/Resources/ShelfResource.php
+++ app/Filament/Resources/ShelfResource.php
@@ -19,7 +19,7 @@ class ShelfResource extends Resource
 {
     protected static ?string $model = Shelf::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-table';
 
     public static function form(Form $form): Form
     {
