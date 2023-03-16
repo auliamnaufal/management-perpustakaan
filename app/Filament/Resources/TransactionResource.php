705b14b829cbe36f0a6c1374161067db09501497
auliamnaufal <auliamnaufal@gmail.com>
2023-03-16 10:10:06 +0700

    Feature: change transaction menu icon to clipboard

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -17,7 +17,7 @@ class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard';
 
     public static function form(Form $form): Form
     {
