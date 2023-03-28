a88bf37b23ee005e28b7c0d0d6c9f331e0167c77
auliamnaufal <auliamnaufal@gmail.com>
2023-03-28 14:22:33 +0700

    Feature: add spacing between nav and detail

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-md-10">
                 <div class="row">
