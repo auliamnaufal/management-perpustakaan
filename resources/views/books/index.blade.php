795bbd3e4e4bfa8641731812cef96ee9f313eb25
auliamnaufal <auliamnaufal@gmail.com>
2023-03-28 14:22:54 +0700

    Refactor: change styling for list books

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,26 +3,21 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="row">
-                    @foreach($books as $book)
-                        <div class="col-lg-4">
-                            <a href="{{ route('books.show', ['id' => $book->id]) }}" class="text-decoration-none">
-                                <div class="card border-0 p-2 mb-5">
-                                    <div class="card-body">
-                                        <img class="w-100 h-25 img-fluid card-img object-fit-cover" src="{{ $book->cover }}"
-                                             alt="{{ $book->title }} cover image">
-                                        <h2>{{ $book->title }}</h2>
-                                        <p>{{ $book->shelf->name }}</p>
-                                        <p>{{ $book->stock }}</p>
-                                    </div>
-                                </div>
-                            </a>
+            @foreach($books as $book)
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12">
+                    <a href="{{ route('books.show', ['id' => $book->id]) }}" class="text-decoration-none text-black">
+                        <div class="card border-0 p-2 mb-5 bg-default">
+                            <div class="card-body">
+                                <img class="w-100 h-50 mb-2 img-fluid card-img object-fit-cover" src="{{ $book->cover }}"
+                                     alt="{{ $book->title }} cover image">
+                                <h2 class="h4 text-truncate">{{ $book->title }}</h2>
+                                <p class="text-secondary mb-4">{{ $book->shelf->name }}</p>
+                                <p class="text-secondary">Stock: {{ $book->stock }}</p>
+                            </div>
                         </div>
-                    @endforeach
+                    </a>
                 </div>
-
-            </div>
+            @endforeach
         </div>
     </div>
 @endsection
