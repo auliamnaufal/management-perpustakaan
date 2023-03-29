4a3146d24f9fcf264990b5b9c2b3e769edd04a5b
auliamnaufal <auliamnaufal@gmail.com>
2023-03-29 11:04:52 +0700

    Refactor: set min-height to card image

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -8,7 +8,7 @@
                     <a href="{{ route('books.show', ['id' => $book->id]) }}" class="text-decoration-none text-black">
                         <div class="card border-0 p-2 mb-5">
                             <div class="card-body">
-                                <img class="w-100 h-50 mb-2 img-fluid card-img object-fit-cover" src="{{ $book->cover }}"
+                                <img class="w-100 h-50 mb-2 img-fluid card-img" style="min-height: 195px; object-fit: cover; object-position: center" src="{{ $book->cover }}"
                                      alt="{{ $book->title }} cover image">
                                 <h2 class="h4 text-truncate">{{ $book->title }}</h2>
                                 <p class="text-secondary mb-4">{{ $book->shelf->name }}</p>
