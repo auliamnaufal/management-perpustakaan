ae93f62d120564ff0a1d0522444b606e7543c344
auliamnaufal <auliamnaufal@gmail.com>
2023-04-05 11:53:46 +0700

    Style: add background color to book item

--- resources/views/components/book-card-item.blade.php
+++ resources/views/components/book-card-item.blade.php
@@ -3,7 +3,7 @@
 ])
 
 <a href="{{ route('books.show', ['id' => $book->id]) }}" class="text-decoration-none text-black">
-    <div class="card border-0 p-2 mb-5">
+    <div class="card border-0 p-2 mb-5" style="background-color: #f2f5f3">
         <div class="card-body">
             <img class="w-100 h-50 mb-2 img-fluid card-img" style="min-height: 195px; object-fit: cover; object-position: center" src="{{ str_starts_with($book->cover, 'http') ? $book->cover : asset('storage/' . $book->cover) }}"
                  alt="{{ $book->title }} cover image">
